fa88e3ea7441d386cabf86f31d9d1b4084de2b93
marius-perera <marius.perera@xero.com>
2020-11-05 15:35:47 +1100

    Change the app text

--- src/App.test.js
+++ src/App.test.js
@@ -6,9 +6,3 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/Hi there!!!/i);
   expect(linkElement).toBeInTheDocument();
 });
-
-test('renders Edit2', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Edit/i);
-  expect(linkElement).toBeInTheDocument();
-});
